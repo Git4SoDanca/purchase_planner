58210f913053cd46df9ffdac47126025c01997b0
fpsoares <fprofeta81@gmail.com>
2018-06-20 11:48:03 -0400

    Added sold last year function

--- PurchaseOrder_Scheduler/SQL/CREATE_purchase_planner_functions_views.sql
+++ PurchaseOrder_Scheduler/SQL/CREATE_purchase_planner_functions_views.sql
@@ -299,6 +299,38 @@ $BODY$;
 ALTER FUNCTION public.sd_qs(integer, date, date)
     OWNER TO purchase_planner;
 
+    -- Quantity Sold Last year
+    CREATE OR REPLACE FUNCTION public.sd_qs_prev_yr(
+    	pid integer,
+    	start_date date,
+    	end_date date)
+        RETURNS numeric
+        LANGUAGE 'sql'
+
+        COST 100
+        VOLATILE
+    AS $BODY$
+
+    SELECT
+    	CASE
+        	WHEN sum(stock_move.product_qty) != 0 THEN sum(stock_move.product_qty) ELSE 0 END AS on_order_total
+    FROM stock_move
+    WHERE
+    	stock_move.location_dest_id = 9
+        AND stock_move.location_id = 12
+        AND (stock_move.state::text = 'done'::character varying)
+        AND date_expected >= ($2 - interval '1 year') --start_date
+        AND date_expected < ($3 - interval '1 year') --end_date
+        AND stock_move.product_id = $1
+    GROUP BY stock_move.product_id
+
+    $BODY$;
+
+    ALTER FUNCTION public.sd_qs(integer, date, date)
+        OWNER TO purchase_planner;
+
+
+
 -- Quantity on hand
 CREATE OR REPLACE FUNCTION sd_qoh(pid int) RETURNS decimal AS
 $$
