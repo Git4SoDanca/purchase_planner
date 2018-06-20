cf52c6debc8c4eb9599a1573e5d17809fb30f31b
fpsoares <fprofeta81@gmail.com>
2018-06-20 17:20:39 -0400

    changed statement from create to create or replace

--- PurchaseOrder_Scheduler/main_scheduler.py
+++ PurchaseOrder_Scheduler/main_scheduler.py
@@ -728,7 +728,7 @@ def create_functions(conn,companycode):
 
 	functions_query[5] = """
 		-- Sales trend
-		CREATE FUNCTION sd_sales_trend(pid int) RETURNS decimal AS
+		CREATE OR REPLACE FUNCTION sd_sales_trend(pid int) RETURNS decimal AS
 		$$
 		SELECT round(sd_qs($1,(now()-'6 months'::interval)::date, now()::date)/sd_qs($1,(now()- '18 months'::interval)::date,(now()- '12 months'::interval)::date)*100,2) as growth;
 		$$ LANGUAGE SQL;
